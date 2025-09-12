e93a1d8207044bac530d09ec4127174095b531a0
LuisOliveira-Dev <luishenrique.oliveira0598@gmail.com>
2025-09-12 18:33:46 -0300

    aprendizado v1

---
+++ aprendizado.py
@@ -0,0 +1,55 @@
+
+#essa declaração de variáveis contém principios de atribuição, sendo essas: int, float & operadores l
+
+nome = 'Luis Henrique'
+sobrenome = 'Oliveira da Silva'
+idade = 19
+ano_nascimento = 2005
+maior_de_idade = idade > 18
+altura_em_metros = 1.80
+
+#essas variáveis contém uma operação matemática com principios de operadores aritméticos
+
+conta_1 = (1 + 1) ** (5 + 5) #'1024'
+conta_2 = 1 + 1 ** 5 + 5 #'7'
+
+"""
+Cálculo IMC : IMC = peso / altura ** 2
+
+Argumentos:
+    peso (float): Peso em quilogramas.
+    altura(float): Altura em metros.
+
+Retorno: 
+    float: Valor do IMC.
+"""
+altura = 1.80
+peso = 65
+imc = peso / (altura ** 2)
+imc = round(imc, 2)
+
+#Print referente as variaveis de int, float e operadores lógicos 
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano nascimento:', ano_nascimento)
+print('Maior de idade:', maior_de_idade)
+print('Altura em metros: {:.2f}'.format(altura_em_metros))
+
+print(' ')
+
+#Print referente a variável de operadores aritméticos 
+
+print('Resultado:',conta_1)
+print('Resultado:',conta_2)
+
+print(' ')
+
+#Print referente ao imc
+
+print('IMC:', imc)
+print('"Já sei!"')
+
+
+
