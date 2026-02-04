784c32f5297d98c9b04fc77f6c0a2683fc33f5b8
LuisOliveira-Dev <luishenrique.oliveira0598@gmail.com>
2026-02-04 00:59:53 -0300

    Tipos Primitivos

---
+++ basico/TiposPrimitivos/int_float.py
@@ -0,0 +1,11 @@
+#int -> números literais inteiros
+#print(11)
+#print(0)
+
+#float -> números com ponto flutuante
+#float sem sinal é considerado positivo
+#print(1.1, 10.11)
+
+#função type -> mostra o tipo que o Python inferiu ao valor
+print(type('Henrique'))
+print(type(1.1), type(-1.1), type(0.0))
