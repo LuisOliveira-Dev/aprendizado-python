7081f6dd97ab7207d3e3be35750788ed5c46939e
LuisOliveira-Dev <luishenrique.oliveira0598@gmail.com>
2026-02-04 22:10:40 -0300

    convertion

---
+++ basico/convertion.py
@@ -0,0 +1,11 @@
+#polimorfismo: utiliza o mesmo operador pra finalidades diferentes
+#concatenação: junção de 2 coisas
+#str, int, float, bool: tipos imutáveis
+#type:mostra o tipo do valor
+print(int('1'), type(int('1')))#conversão de string pra inteiro
+
+print(type(float('1') + 1))#converte string pra float
+
+print(bool(' '))#é uma string com espaço
+
+print(str(11) + 'b') #concatenação 
