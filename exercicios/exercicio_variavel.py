e0fae102abe161cb5f75ec45cdeaba358f108e10
LuisOliveira-Dev <luishenrique.oliveira0598@gmail.com>
2026-02-15 22:32:24 -0300

    ExerciciosFolder

---
+++ exercicios/exercicio_variavel.py
@@ -0,0 +1,16 @@
+#váriaveis
+nome = 'Luis Henrique'
+sobrenome = 'Oliveira'
+idade = 20
+ano_nascimento = 2005
+maiorIdade = idade >= 18
+altura_emMetros = 1.80
+
+#Print
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('ano_nascimento:', ano_nascimento)
+print('maioridade:', maiorIdade)
+print ('Altura:', altura_emMetros)
+
