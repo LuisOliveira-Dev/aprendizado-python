565ce59daa9989da56049103e51c4c2237fd9147
LuisOliveira-Dev <luishenrique.oliveira0598@gmail.com>
2026-02-19 22:54:04 -0300

    Concatenation

---
+++ basico/concatenacao.py
@@ -0,0 +1,8 @@
+#concatenacao é a junção de Strings
+concatenacao = 'Luis' + ' ' + 'Henrique'
+a_dez_vezes = 'A' * 10
+tres_vezes_luis = 3 * 'Luis'
+
+print(concatenacao)
+print(a_dez_vezes)
+print(tres_vezes_luis)
