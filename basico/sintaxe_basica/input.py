95fcdb9632624e70778b53067986ac040e42d071
LuisOliveira-Dev <luishenrique.oliveira0598@gmail.com>
2026-02-22 19:48:28 -0300

    input_v2

--- basico/sintaxe_basica/input.py
+++ basico/sintaxe_basica/input.py
@@ -1,4 +1,13 @@
 #input é a entrada de dados
 
 nome = input('Qual é o seu nome? : ')
+numero_1 = int(input('Digite um número: '))
+numero_2 = int(input('Digite outro número: '))
+soma = numero_1 + numero_2
+
+print(' ')
+
 print(f'O seu nome é {nome}')
+print(f'Primeiro número: {numero_1}')
+print(f'Segundo número: {numero_2}')
+print(f'Resultado da soma: {soma}')
