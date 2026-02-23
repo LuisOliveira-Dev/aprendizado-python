3b3dac60352303de3bb00ac4db90e8d3ddd68734
LuisOliveira-Dev <luishenrique.oliveira0598@gmail.com>
2026-02-23 20:55:43 -0300

    operadores_relacionais

---
+++ basico/sintaxe_basica/operadores_relacionais.py
@@ -0,0 +1,24 @@
+"""
+Operadores de comparação(relacionais)
+
+> maior | Exemplo: 2 > 1
+>= maior ou igual | Exemplo: 2 >= 2
+< menor | Exemplo: 1 < 2
+<= menor ou igual | Exemplo: 2 <= 2
+== igual | Exemplo: 'a' == 'a'
+!= diferente | Exemplo: 'a' != 'b'
+"""
+
+maior = 2 > 1
+maior_ou_igual = 2 >= 2
+menor = 1 < 2
+menor_ou_igual = 2 <= 2
+igual ='a' == 'a'
+diferente = 'a' != 'b'
+
+print('maior:', maior)
+print('maior ou igual:', maior_ou_igual)
+print('menor:',menor)
+print('menor ou igual:', menor_ou_igual)
+print('igual:', igual)
+print('diferente:', diferente)
