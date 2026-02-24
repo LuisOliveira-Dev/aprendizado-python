044aa0592fbb3cdff38e41a565a15a9be6d7f313
LuisOliveira-Dev <luishenrique.oliveira0598@gmail.com>
2026-02-23 22:49:22 -0300

    exercicio_if_comparison-V2

--- exercicios/if_comparison.py
+++ exercicios/if_comparison.py
@@ -1,5 +1,15 @@
-primeiro_valor = input('Primeiro número: ')
-segundo_valor = input('Segundo número: ')
+# Exercício:
+# Crie um programa que peça ao usuário dois números.
+# Em seguida, compare os valores informados.
+# Se o primeiro número for maior que o segundo, exiba:
+# "Primeiro valor maior".
+# Caso contrário, exiba:
+# "Segundo valor maior".
+#
+# Utilize estrutura condicional (if e else) para resolver o exercício.
+
+primeiro_valor = int(input('Primeiro número: '))
+segundo_valor = int(input('Segundo número: '))
 
 if primeiro_valor > segundo_valor:
     print('Primeiro valor maior')
