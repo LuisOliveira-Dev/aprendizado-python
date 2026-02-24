8579aae9f7c78bdeb65c5d710c39d79dcb14576e
LuisOliveira-Dev <luishenrique.oliveira0598@gmail.com>
2026-02-23 22:45:56 -0300

    exercicio_if_comparison

---
+++ exercicios/if_comparison.py
@@ -0,0 +1,7 @@
+primeiro_valor = input('Primeiro número: ')
+segundo_valor = input('Segundo número: ')
+
+if primeiro_valor > segundo_valor:
+    print('Primeiro valor maior')
+else:
+    print('Segundo valor maior')
