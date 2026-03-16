9a8feef9924702029b338d4f22e5e2b3276aa618
LuisOliveira-Dev <luishenrique.oliveira0598@gmail.com>
2026-03-16 17:54:13 -0300

    fatiamento_string_len

---
+++ basico/sintaxe_basica/fatiamento_string.py
@@ -0,0 +1,17 @@
+'''
+Fatiamento de strings
+012345678
+Olá mundo
+-987654321
+Fatiamento [i:f:p] [::]
+i -> inicio
+f -> fim
+p -> passo
+Obs.: a função len retorna a qtd
+de caracteres da str
+índice é diferente de número de caracteres
+'''
+
+variavel = 'olá mundo'
+print (variavel[0:9:2])
+
