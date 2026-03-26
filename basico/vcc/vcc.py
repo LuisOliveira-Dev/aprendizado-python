088b2831667c65965430bd5f25b356f715303723
LuisOliveira-Dev <luishenrique.oliveira0598@gmail.com>
2026-03-25 23:23:32 -0300

    vcc

---
+++ basico/vcc/vcc.py
@@ -0,0 +1,11 @@
+#VCC -> váriavel, constante e complexidade de código
+#Constante = valores que não variam
+#Variável = valores que variam
+
+velocidade = 61 #velocidade atual do carro
+local_carro = 90 #local em que o carro está na estrada
+
+RADAR_1 = 60 #velocidade máxima do radar 1
+LOCAL_1 = 100 #local onde o radar 1 está
+RADAR_RANGE = 101 #A distancia onde o radar pega
+
